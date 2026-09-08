449c79c424f256e97c39373c5d2c2091c4906235
рома килижков <kiliqekovkil@gmail.com>
2026-09-09 03:07:30 +0700

    test: run B04 StrokeMath Studio spec

--- src/server/Bootstrap.server.lua
+++ src/server/Bootstrap.server.lua
@@ -9,6 +9,9 @@ if RunService:IsStudio() then
 	local testsFolder = script.Parent:WaitForChild("Tests")
 	local B03StrokeMathSpec = require(testsFolder:WaitForChild("B03StrokeMathSpec"))
 	B03StrokeMathSpec.run()
+
+	local B04StrokeMathSpec = require(testsFolder:WaitForChild("B04StrokeMathSpec"))
+	B04StrokeMathSpec.run()
 end
 
 print("[DrawRacers] server bootstrap ready")
