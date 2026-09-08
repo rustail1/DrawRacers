f34f90b4958d054e1d309c619dfe6fe0e4224a6e
рома килижков <kiliqekovkil@gmail.com>
2026-09-09 03:00:14 +0700

    test(B03): run StrokeMath spec in Studio

--- src/server/Bootstrap.server.lua
+++ src/server/Bootstrap.server.lua
@@ -5,6 +5,10 @@ local RunService = game:GetService("RunService")
 if RunService:IsStudio() then
 	local M0TestScene = require(script.Parent.M0TestScene)
 	M0TestScene.build()
+
+	local testsFolder = script.Parent:WaitForChild("Tests")
+	local B03StrokeMathSpec = require(testsFolder:WaitForChild("B03StrokeMathSpec"))
+	B03StrokeMathSpec.run()
 end
 
 print("[DrawRacers] server bootstrap ready")
